85d26996f8fa185a472c08da6a77ee9d38644e86
Adelson Developer <desouzaadelson@hotmail.com>
2023-05-11 17:27:00 -0300

    classe1

--- objeto/classe1.js
+++ objeto/classe1.js
@@ -81,24 +81,57 @@ class CicloFinanceiro {
   *   const todasAsPessoas = [ ...pessoas, 'Marcus', 'Tiago']
   *   console.log(todasAsPessoas)  // [ 'Pedro', 'Maria', 'João', 'Marcus', 'Tiago' ]
   * 
+  * ? Array.prototype.push()
+  * O método push() adiciona um ou mais elementos ao final de um array e retorna o novo comprimento desse array.
+  * 
+  * TODO See the links:
+  * 
+  ** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/Array/push
+  *
+  ** https://www.w3schools.com/js/js_array_methods.asp
+
+  ** Example of push():
+  * 
+  ** var numeros = [1, 2, 3];
+  ** numeros.push(4);
+  * 
+  ** console.log(numeros); // [1, 2, 3, 4]
+  * 
+  ** numeros.push(5, 6, 7);
+  ** console.log(numeros); // [1, 2, 3, 4, 5, 6, 7]
+  * 
   */
+
+  /**
+   *  A função addLancamentos recebe um ou mais lancamentos como parâmetros através do operador ( ... ).
+   *  O método lamcamentos.forEach(l => this.lancamentos.push(l)) executará a função l => this.lancamentos.push(l)
+   * que lhe foi passada como parâmetro, em cada elemento do array lancamentos.
+   *  A função lancamentos.push(l) adicionará(concatenará) ao final do array (lancamentos), cada novo lancamentos (l) e retornará
+   * o tamanho(lenght) do array.
+   *  O método abaixo pega cada elemento concatenado dentro do array e dá 
+   * um push()
+   */
     addLancamentos(...lancamentos) { 
         lancamentos.forEach(l => this.lancamentos.push(l))
     }
-
+/**
+ * 
+ * 
+ */
     sumario() {
         let valorConsolidado = 0
-        this.lancamentos.forEach( l => {
-            valorConsolidado += l.valor
+        this.lancamentos.forEach( l => { //* l é o valor corrente, atual
+            valorConsolidado += l.valor //* Cada valor lançado vai increentoado ao valor consolidado
         })
          
         return valorConsolidado
     }
 }
 
-const salario = new Lancamento('Salario', 45000)
+const salario = new Lancamento('Salario', 45000) // Instanciando a classe Lancamento através do operador new
 const contaDeLuz = new Lancamento('Luz', -220)
 
 const contas = new CicloFinanceiro(6, 2018)
 contas.addLancamentos(salario, contaDeLuz)
 console.log(contas.sumario())
+
