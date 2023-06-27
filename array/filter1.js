bd45d09449db042ebf8af84a9590e3663e915f92
Adelson Developer <desouzaadelson@hotmail.com>
2023-06-27 20:37:52 -0300

    filter1 with others functions

--- array/filter1.js
+++ array/filter1.js
@@ -21,7 +21,7 @@
     { nome: 'Copo de Plástico', preco: 18.99, fragil: true }
  ]
 
- console.log(produtos.filter(function(p) {
+ console.log(produtos.filter(function(p) { //return [] 
     return false
  }))
 
@@ -29,10 +29,26 @@ const caro = produtos => produtos.preco >= 500
 const fragil = produtos => produtos.fragil 
 
 console.log(produtos.filter(caro).filter(fragil))
-
+/**
+ * Returns 
+ * [
+ *   { nome: 'Notebook', preco: 2499, fragil: true },
+ *   { nome: 'iPad Pro', preco: 4199, fragil: true }
+ * ]
+ * 
+ */
 const ateCemReais = produtos.filter(produtos => produtos.preco <= 100)
 
 const produtoName = produtos.filter(produtos => produtos.nome === 'Notebook')
 
- console.log(ateCemReais)
- console.log(produtoName)
+console.log(ateCemReais)
+/**
+ * Return
+ *  [
+ *   { nome: 'Copo de Vidro', preco: 12.49, fragil: true },
+ *   { nome: 'Copo de Plástico', preco: 18.99, fragil: true }
+ * ]
+ * 
+ */
+
+ console.log(produtoName) //[ { nome: 'Notebook', preco: 2499, fragil: true } ]
