cc4b09e8565befd4e19e35f776d22eb32ffb643a
Adelson Developer <desouzaadelson@hotmail.com>
2023-06-27 19:08:53 -0300

    map3

--- array/map3.js
+++ array/map3.js
@@ -1,4 +1,4 @@
-// AULA 137 - map2
+// AULA 138 - map3
 
 /**
  * TODO: See the links:
@@ -7,7 +7,16 @@
  ** https://developer.mozilla.org/pt-BR/docs/Web/JavaScript/Reference/Global_Objects/JSON/parse
  */
 
-const carrinho = [
+// Criando, implementando o map2()
+Array.prototype.map2 = function(callback) {
+    const newArray = [];    
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i, this))
+    }
+    return newArray;
+}
+
+ const carrinho = [
     '{ "nome": "Borracha", "preco": 3.45 }',
     '{ "nome": "Caderno", "preco": 13.90 }',
     '{ "nome": "Kit de Lapis", "preco": 41.22 }',
@@ -18,9 +27,6 @@ const paraObjeto = json =>JSON.parse(json) // convert text to object
 const apenasPreco = produto => produto.preco // return just preco
 
 // Retornar um array apenas com os preços(), utilizando o método map()
-const resultado = carrinho.map(paraObjeto).map(apenasPreco) // convert carrinho to object and return just apenasPreco
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco) // convert carrinho to object and return just apenasPreco
 console.log(resultado) //* return [ 3.45, 13.9, 41.22, 7.5 ]
-
-
-
 
